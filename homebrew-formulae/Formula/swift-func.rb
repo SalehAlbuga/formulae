31bd4383ac55d5bc0657349902484022e6ec1428
Saleh Albuga <samalbuga@outlook.com>
2019-11-24 23:12:41 +0200

    update to 0.1.3

--- homebrew-formulae/Formula/swift-func.rb
+++ homebrew-formulae/Formula/swift-func.rb
@@ -1,7 +1,7 @@
 class SwiftFunc < Formula
     desc "Azure Functions for Swift CLI tools"
     homepage "https://github.com/SalehAlbuga/azure-functions-swift-tools"
-    url "https://github.com/SalehAlbuga/azure-functions-swift-tools.git", :tag => "v0.1.2"
+    url "https://github.com/SalehAlbuga/azure-functions-swift-tools.git", :tag => "v0.1.3"
     sha256 "1be333b8311a1b1504804025e3f57a8fac0317fdecfe9ccb404ef4297581e5fe"
 
     depends_on :xcode => ["11.0", :build]
